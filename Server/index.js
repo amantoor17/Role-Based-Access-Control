522dda08ee798a8059674a66c7211808a55c619c
amantoor17 <amantoordpps@gmail.com>
2024-11-28 14:29:31 +0530

    cors-2

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Allow requests from your frontend
 app.use(cors({
-    origin: "https://role-based-access-control-red.vercel.app/", // Allow this specific origin
+    origin: "https://role-based-access-control-navy.vercel.app/", // Allow this specific origin
     methods: "GET,POST,PUT,DELETE", // Specify allowed methods
     credentials: true // Include credentials if needed (e.g., cookies)
 }));
